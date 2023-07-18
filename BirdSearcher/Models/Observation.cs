5baec357df8e1171d2bf3bf932350897dca509a0
Marcin Kirsz <m.kirsz93@gmail.com>
2023-07-18 18:02:27 +0200

    Add project files.

---
+++ BirdSearcher/Models/Observation.cs
@@ -0,0 +1,10 @@
+﻿namespace BirdSearcher.Models
+{
+    public class Observation
+    {
+        public string comName { get; set; }
+        public DateTime obsDt { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+}
---
+++ BirdSearcher/Services/ObservationService.cs
@@ -0,0 +1,29 @@
+﻿namespace BirdSearcher.Services;
+
+using BirdSearcher.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+public class ObservationService
+{
+    private readonly HttpClient _httpClient;
+
+    public ObservationService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Observation>> GetRecentObservationsByRegionCode(string regionCode)
+    {
+        string apiKey = "715f6g7c93hr";
+        var response = await _httpClient.GetAsync($"https://api.ebird.org/v2/data/obs/{regionCode}/recent?key={apiKey}");
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var observations = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Observation>>(responseContent);
+
+        return observations;
+    }
+
+}
+
