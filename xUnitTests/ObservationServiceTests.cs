853bf3114e96cda08e7f211ff28ca11608e86f3e
Marcin Kirsz <m.kirsz93@gmail.com>
2023-07-19 15:27:40 +0200

    added unit test project

---
+++ xUnitTests/ObservationServiceTests.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using Xunit;
+using BirdSearcher.Models;
+using BirdSearcher.Services;
+
+public class ObservationServiceTests
+{
+    [Fact]
+    public async Task GetRecentObservationsByRegionCode_Should_Return_Observations()
+    {
+        // Arrange
+        var expectedObservations = new List<Observation>
+        {
+            new Observation { comName = "Robin", obsDt = DateTime.Now },
+            new Observation { comName = "Sparrow", obsDt = DateTime.Now }
+        };
+
+        //Mock HTTP client
+        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        mockHttpMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage { 
+                StatusCode = HttpStatusCode.OK, 
+                Content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(expectedObservations))
+            });
+
+        var client = new HttpClient(mockHttpMessageHandler.Object);
+
+        var observationService = new ObservationService(client);
+
+        // Act
+        var result = await observationService.GetRecentObservationsByRegionCode("PL", "myApiKey");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedObservations.Count, result.Count);
+    }
+}
